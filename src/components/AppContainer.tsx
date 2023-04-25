a8350434056443cf55c7649bfe04a80a3b6cbb52
Frehiwot Haile <54357776+she-code@users.noreply.github.com>
2023-04-25 08:44:22 +0530

    initail setup

---
+++ src/components/AppContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AppContainer() {
+  return (
+    <div className="mx-auto w-1/2 shadow-md text-center text-2xl my-6 p-5">
+      Welcome
+    </div>
+  );
+}
