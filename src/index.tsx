871a9df88111655914f4c04d0b9a0e5f0fbcb8f8
Frehiwot Haile <54357776+she-code@users.noreply.github.com>
2023-06-12 11:34:52 +0530

    added CRA config

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import * as Sentry from "@sentry/react";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import "./index.css";
 import App from "./App";
@@ -38,6 +39,7 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
