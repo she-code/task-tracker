5a3a51be2fbc66047ce058e40ae9a5b5da3ece73
Frehiwot Haile <54357776+she-code@users.noreply.github.com>
2024-03-12 21:23:31 +0530

    updating to production

--- src/index.tsx
+++ src/index.tsx
@@ -17,21 +17,23 @@ if (process.env.REACT_APP_SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
     integrations: [
-      new Sentry.BrowserTracing({
-        tracePropagationTargets: [
-          "localhost",
-          /^https:\/\/yourserver\.io\/api/,
-        ],
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false,
       }),
-      new Sentry.Replay(),
     ],
     // Performance Monitoring
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
     // Session Replay
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   });
 }
+
+root.render(<App />);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -45,3 +47,21 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Sentry.init({
+//   dsn: process.env.REACT_APP_SENTRY_DSN,
+//   integrations: [
+//     new Sentry.BrowserTracing({
+//       tracePropagationTargets: [
+//         "localhost",
+//         /^https:\/\/yourserver\.io\/api/,
+//       ],
+//     }),
+//     new Sentry.Replay(),
+//   ],
+//   // Performance Monitoring
+//   tracesSampleRate: 1.0,
+//   // Session Replay
+//   replaysSessionSampleRate: 0.1,
+//   replaysOnErrorSampleRate: 1.0,
+// });
